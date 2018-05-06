e4249b627f650b50300de221aa09d1f6ba238094
CntrZed <jc3d@live.com>
2018-05-06 14:55:52 -0700

    Uploaded

---
+++ Systems/Assets/C#/Door_RandJigle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class Door_RandJigle : MonoBehaviour {
+
+    [Serializable]
+    public class DoorSettinsOption
+    {
+        [SerializeField]
+        public float openingSpeed = 1;
+        public float closingSpeed = 1.3f;
+        [Range(0, 1)]
+        public float closeStartFrom = 0.6f;
+        
+        public KeyCode openButton = KeyCode.E; //Button on the keyboard to open the door
+        public bool autoClose = false; //Automatically close the door. Forced to true when in AUTOMATIC mode.
+    }
+    public DoorSettinsOption controls = new DoorSettinsOption ();
+
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Systems/Assets/C#/OnGameStart_Disable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGameStart_Disable : MonoBehaviour
+{
+    
+
+    void Awake()
+    {
+        this.gameObject.SetActive(false);//When game start get the gameobject that this script is attached to and disable it.
+        
+    }
+
+}
